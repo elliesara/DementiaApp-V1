c90f0965161c8992cfa8a4a47c7348db463aa8a4
Jacob Yu <yujacob100@gmail.com>
2020-09-13 02:19:14 -0700

    Updated MentalSymptoms to include Core Data

--- Dementia-App/Symptoms/MentalSymptoms.swift
+++ Dementia-App/Symptoms/MentalSymptoms.swift
@@ -9,18 +9,27 @@
 import SwiftUI
 
 struct MentalSymptoms: View {
-
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.presentationMode) var presentation
     @State private var newSymptom: Bool = false
+    private let mSymptoms = ["Disturbed thought process",
+                                "Chronic confusion",
+                                "Decreased ability to reason",
+                                "Decreased attention span"]
+    @State var mChecks = [false, false, false, false]
 
-    @State private var decreasedAbilityToReason: Bool = false
-    @State private var inabilityToCalculate: Bool = false
-    @State private var memoryLoss: Bool = false
-    @State private var decreasedAttentionSpan: Bool = false
-    @State private var easyDistractibility: Bool = false
-    @State private var lackOfInhibitions: Bool = false
-    @State private var inappropriateSocialBehavior: Bool = false
-    @State private var paranoia: Bool = false
-    @State private var explosiveBehavior: Bool = false
+//    @State private var newSymptom: Bool = false
+//
+//    @State private var decreasedAbilityToReason: Bool = false
+//    @State private var inabilityToCalculate: Bool = false
+//    @State private var memoryLoss: Bool = false
+//    @State private var decreasedAttentionSpan: Bool = false
+//    @State private var easyDistractibility: Bool = false
+//    @State private var lackOfInhibitions: Bool = false
+//    @State private var inappropriateSocialBehavior: Bool = false
+//    @State private var paranoia: Bool = false
+//    @State private var explosiveBehavior: Bool = false
+    
 
     init(){
         UITableView.appearance().backgroundColor = #colorLiteral(red: 0.7541411519, green: 0.8313546777, blue: 0.9701576829, alpha: 1)
@@ -42,15 +51,35 @@ struct MentalSymptoms: View {
                         .padding(.top, geometry.size.height*0.013)
 
                     List {
-                        HStack { Text("Having an unkempt appearance"); Spacer(); CheckMark() }
-                        HStack { Text("Inability to wash body"); Spacer(); CheckMark() }
-                        HStack { Text("Difficulty choosing clothing"); Spacer(); CheckMark() }
-                        HStack { Text("Difficulty brushing teeth"); Spacer(); CheckMark() }
-                        HStack { Text("Trouble with wearing clothes"); Spacer(); CheckMark() }
-                        HStack { Text("Inability to sit on toilet"); Spacer(); CheckMark() }
-                        HStack { Text("Difficulty Washing Hands"); Spacer(); CheckMark() }
-                        HStack { Text("Inability to Shave/Comb Hair"); Spacer(); CheckMark() }
-                        HStack { Text("Convulsions"); Spacer(); CheckMark() }
+                        ForEach(0..<self.mChecks.count) { i in
+                            HStack {
+                                Text(self.mSymptoms[i])
+                                Spacer()
+                                Button(action: {
+                                    self.mChecks[i].toggle()
+                                    print(self.mChecks[i])
+                                }) {
+                                    if self.mChecks[i] {
+                                        Image(systemName: "checkmark.square.fill")
+                                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0.5492870212, blue: 1, alpha: 1)))
+                                            .font(.system(size: UIScreen.main.bounds.width*0.06))
+                                    } else {
+                                        Image(systemName: "square.fill")
+                                            .foregroundColor(Color(#colorLiteral(red: 0.9339778938, green: 0.9339778938, blue: 0.9339778938, alpha: 1)))
+                                            .font(.system(size: UIScreen.main.bounds.width*0.06))
+                                    }
+                                }
+                            }
+                        }
+//                        HStack { Text("Having an unkempt appearance"); Spacer(); CheckMark() }
+//                        HStack { Text("Inability to wash body"); Spacer(); CheckMark() }
+//                        HStack { Text("Difficulty choosing clothing"); Spacer(); CheckMark() }
+//                        HStack { Text("Difficulty brushing teeth"); Spacer(); CheckMark() }
+//                        HStack { Text("Trouble with wearing clothes"); Spacer(); CheckMark() }
+//                        HStack { Text("Inability to sit on toilet"); Spacer(); CheckMark() }
+//                        HStack { Text("Difficulty Washing Hands"); Spacer(); CheckMark() }
+//                        HStack { Text("Inability to Shave/Comb Hair"); Spacer(); CheckMark() }
+//                        HStack { Text("Convulsions"); Spacer(); CheckMark() }
                     }.foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1))).frame(width: UIScreen.main.bounds.width*0.9)
 
                     Button(action: { self.newSymptom = true}) {
@@ -78,12 +107,26 @@ struct MentalSymptoms: View {
                     }
 
                     Spacer()
-                }
+                }.navigationBarItems(trailing:
+                Button("Submit") { self.submitButton() }
+                )
+            }
+        }
+    }
+    func submitButton() {
+        /// func and var names are lowercase
+        for i in 0..<mChecks.count {
+            print("\(i): \(mChecks[i])")
+            if mChecks[i] == true {
+                let mSymptom = MSymptomEntity(context: self.managedObjectContext)
+                mSymptom.mSymptomName = mSymptoms[i]
+                mSymptom.mCreatedAt = Date()
+                mSymptom.mCheckedState = mChecks[i]
+                CoreDataManager.shared.saveContext()
             }
         }
     }
 }
-
 
 struct MentalSymptoms_Previews: PreviewProvider {
     static var previews: some View {
