ee6d5343ead440bb732673b537aa1f1cb06776af
Eaton Huang <eatonstephenhuang@gmail.com>
2020-09-19 18:50:52 -0700

    Got core data to work with social symptoms. Yayeet.

--- Dementia-App/Symptoms/SSymptomsView.swift
+++ Dementia-App/Symptoms/SSymptomsView.swift
@@ -1,5 +1,5 @@
 //
-//  SSymptomsView.swift
+//  SocialSymptoms.swift
 //  Dementia-App
 //
 //  Created by Ellie Sara Huang on 7/10/20.
@@ -8,22 +8,25 @@
 
 import SwiftUI
 
-struct SSymptomsView: View {
+struct SocialSymptoms: View {
     
-    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.presentationMode) var presentation
     @State private var newSymptom: Bool = false
+        
+    private let sSymptoms = ["Inability to speak properly",
+                             "Slurring",
+                             "Inability to identify object",
+                             "Difficulty forming sentences",
+                             "Cognitive impairment",
+                             "Hostile Behavior",
+                             "Isolation from others",
+                             "Unhealthy reliance on others",
+                             "Abnormal Stress",
+                             "Abnormal Fatigue"]
     
-    @State private var unkempt: Bool = false
-    @State private var washBody: Bool = false
-    @State private var clothing: Bool = false
-    @State private var teeth: Bool = false
-    @State private var wearing: Bool = false
-    @State private var toilet: Bool = false
-    @State private var hands: Bool = false
-    @State private var hair: Bool = false
-    @State private var convulsion: Bool = false
+    @State var sChecks = [false, false, false, false, false, false, false, false, false, false]
     
-    /// needed to make the List match the background color
     init(){
         UITableView.appearance().backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.8804875016, blue: 0.7963053584, alpha: 1)
         UITableViewCell.appearance().backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.8804875016, blue: 0.7963053584, alpha: 1)
@@ -33,67 +36,89 @@ struct SSymptomsView: View {
     var body: some View {
         GeometryReader { geometry in
             
-            NavigationView {
-                
-                ZStack {
-                    Color(#colorLiteral(red: 0.9529411765, green: 0.8804875016, blue: 0.7963053584, alpha: 1))
-                        .edgesIgnoringSafeArea(.all)
+            ZStack {
+                Color(#colorLiteral(red: 0.9529411765, green: 0.8804875016, blue: 0.7963053584, alpha: 1))
+                    .edgesIgnoringSafeArea(.all)
                     
-                    VStack {
+                VStack {
+                    
+                    Text("Social Symptoms").font(.largeTitle).fontWeight(.bold).foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
+                    Text("Select all that apply").font(.caption).foregroundColor(Color.blue)
+                        .padding(.top, geometry.size.height*0.013)
                         
-                        Text("Social Symptoms").font(.largeTitle).fontWeight(.bold).foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
-                        Text("Select all that apply").font(.caption).foregroundColor(Color.blue)
-                            .padding(.top, geometry.size.height*0.013)
-                        
-                        List {
-                            HStack { Text("Fatigue"); Spacer(); CheckMark() }
-                            HStack { Text("Anxiety"); Spacer(); CheckMark() }
-                            HStack { Text("Financial Insecurity"); Spacer(); CheckMark() }
-                            HStack { Text("Uncommunicative"); Spacer(); CheckMark() }
-                            HStack { Text("Withdrawn"); Spacer(); CheckMark() }
-                            HStack { Text("Cognitive Impairment"); Spacer(); CheckMark() }
-                            HStack { Text("Impaired Sleep Pattern"); Spacer(); CheckMark() }
-                            HStack { Text("Hostile Behavior"); Spacer(); CheckMark() }
-                            HStack { Text("Isolation From Others"); Spacer(); CheckMark() }
-                        }.foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1))).frame(width: UIScreen.main.bounds.width*0.9)
-                        
-                        Button(action: { self.newSymptom = true }) {
-                            
-                            HStack(alignment: .center) {
-                                Image(systemName: "plus")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: geometry.size.width*0.05, height: geometry.size.height*0.05)
-                                
-                                Text("New Symptom")
-                                    .font(.system(size: geometry.size.width*0.048))
-                                    .fontWeight(.bold)
+                    List {
+                        ForEach(0..<self.sChecks.count) { i in
+                            HStack {
+                                Text(self.sSymptoms[i])
+                                Spacer()
+                                Button(action: {
+                                    self.sChecks[i].toggle()
+                                    print(self.sChecks[i])
+                                }) {
+                                    if self.sChecks[i] {
+                                        Image(systemName: "checkmark.square.fill")
+                                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0.5492870212, blue: 1, alpha: 1)))
+                                            .font(.system(size: UIScreen.main.bounds.width*0.06))
+                                    } else {
+                                        Image(systemName: "square.fill")
+                                            .foregroundColor(Color(#colorLiteral(red: 0.9339778938, green: 0.9339778938, blue: 0.9339778938, alpha: 1)))
+                                            .font(.system(size: UIScreen.main.bounds.width*0.06))
+                                    }
+                                }
                             }
-                            .padding()
-                            .foregroundColor(Color.white)
-                            .frame(width: geometry.size.width*0.5, height: geometry.size.height*0.063)
-                            .background(Color(#colorLiteral(red: 0.7907425724, green: 0.72770939, blue: 0.6657392306, alpha: 1)))
-                            .cornerRadius(10)
-                            
-                        }.sheet(isPresented: self.$newSymptom) {
-                            NewSymptom()
                         }
+                    }.foregroundColor(Color(#colorLiteral(red: 0.2928513885, green: 0.2821008563, blue: 0.2951488495, alpha: 1)))
+                     .frame(width: UIScreen.main.bounds.width*0.9)
                         
-                        Spacer()
+                    Button(action: { self.newSymptom = true }) {
+
+                        HStack(alignment: .center) {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: geometry.size.width*0.05, height: geometry.size.height*0.05)
+
+                            Text("New Symptom")
+                                .font(.system(size: geometry.size.width*0.048))
+                                .fontWeight(.bold)
+                        }
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .frame(width: geometry.size.width*0.5, height: geometry.size.height*0.063)
+                        .background(Color(#colorLiteral(red: 0.7907425724, green: 0.72770939, blue: 0.6657392306, alpha: 1)))
+                        .cornerRadius(10)
+
+                    }.sheet(isPresented: self.$newSymptom) {
+                        NewSymptom()
+                    }
+                    
+                    Spacer()
                         
-                    }.navigationBarItems(leading:
-                        Button("Cancel") { self.presentationMode.wrappedValue.dismiss()}, trailing:
-                        Button("Submit") { self.presentationMode.wrappedValue.dismiss() }
-                    )
                 }
-                
+                .navigationBarItems(trailing:
+                    Button("Submit") { self.submitButton() }
+                )
             }
         }
     }
+    
+    func submitButton() {
+        for i in 0..<sChecks.count {
+            print("\(i): \(sChecks[i])")
+            if sChecks[i] == true {
+                let sSymptom = SSymptomEntity(context: self.managedObjectContext)
+                sSymptom.sSymptomName = sSymptoms[i]
+                sSymptom.sCreatedAt = Date()
+                sSymptom.sCheckedState = sChecks[i]
+                CoreDataManager.shared.saveContext()
+            }
+        }
+    }
+    
 }
 
-struct SSymptomsView_Previews: PreviewProvider {
+struct SocialSymptoms_Previews: PreviewProvider {
     static var previews: some View {
-        SSymptomsView()
+        SocialSymptoms()
     }
 }
